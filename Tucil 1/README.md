03f8b127e992c94c114ca90a0455274e83d0c763
addinnabilal <16520342@std.stei.itb.ac.id>
2022-01-26 08:36:08 +0700

    Submission

---
+++ Tucil 1/README.md
@@ -0,0 +1,27 @@
+# Algeo02-20016 - Web-based Image Compression using SVD Algorithm
+
+<!-- ## Table of Contents
+* [General Info](#general-information)
+* [Usage](#usage)
+* [Contact](#contact)
+
+
+
+## General Information
+A 2D word search program using C++ brute-force algorithm.
+
+## Devide Requirements
+1. g++ compiler installed
+2. c++ programming languange
+
+## How to run?
+(LOCAL)
+1. (First use only): Clone this repository, then change directory to src. `cd bin`
+2. Run the program by writing `./main`
+3. Insert you configuration file name
+
+## Contact
+Created by:
+Addin Nabilal Huda
+Informatics Engineering/Computer Science, Bandung Institute of Technology
+
---
+++ Tucil 1/src/main.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <ratio>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+// deklarasi dan inisiasi variabel global
+int xdir[]={1,1,0,-1,-1,-1,0,1};
+int ydir[]={0,-1,-1,-1,0,1,1,1};
+int boardrows, boardcols;
+
+void readFile(string file_name, vector<vector<char>> *board, vector<string> *wordlist ){
+    ifstream file_in(file_name.c_str());
+    if (!file_in){
+        cerr <<"Cannot open the File: " <<file_name<<endl;
+    }    
+    string line;
+    while (getline(file_in, line)) {
+        vector<char> vecline;
+        for (char &c: line) {
+            if (c!=' '){
+                vecline.push_back(c);
+            }
+        }
+        if (vecline.size()>0){
+            (*board).push_back(vecline);
+            }
+        else break;
+    }
+
+    string wordlistline;
+    while (getline(file_in, wordlistline)) {
+        (*wordlist).push_back(wordlistline);
+        }
+    file_in.close();
+}
+
+void printOutput(string word, vector<vector<char>> board, int row, int col, int dir){
+    int i,j,k;
+    for (i=0;i<boardrows;i++){
+        for (j=0;j<boardcols;j++){
+            board[i][j]='-';
+        }
+    }
+    int len=word.size();
+    for (k=0;k<word.size();k++){
+        board[row][col]=word[k];
+        row+=ydir[dir];
+        col+=xdir[dir];
+    }
+    for (i=0;i<boardrows;i++){
+        for (j=0;j<boardcols;j++){
+            cout << board[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+bool search(string word, vector<vector<char>> board, int row, int col, int *dir, int *count){
+    int len = word.size();
+    int i,y,x,d;
+    *count +=1;
+    if (board[row][col]==word[0]){
+        for (d=0;d<8;d++){
+            y=row+ydir[d];
+            x=col+xdir[d];
+            for (i=1;i<len;i++){
+                if (x<0 || x>=boardcols || y<0 || y>=boardrows) break;
+                *count += 1;
+                if (board[y][x]!=word[i]) break;
+                y+=ydir[d];
+                x+=xdir[d];
+            }
+            if (i==len) {
+                *dir=d;
+                return true;
+                }
+            }
+            *dir=0;
+            return false;
+        }
+    *dir=0;
+    return false;
+}
+
+int main(){
+    string file_name;
+    vector<vector<char>> board;
+    vector<string> wordlist;
+    int word, row,col;
+    int dir,count, totalcount=0;
+    double totaldiff=0;
+    bool isAvail;
+
+    cout << "Welcome to 2D word search program!\nInsert file name: ";
+    cin >> file_name;
+    cout << "\nResults:\n";
+    readFile(file_name, &board, &wordlist);
+    boardrows=board.size();
+    boardcols=board[0].size();
+
+    for (word=0;word<wordlist.size();word++){
+        isAvail=false;
+        cout << wordlist[word] << "\n";
+        for (row=0;row<boardrows;row++){
+            for (col=0;col<boardcols;col++){
+                count=0;
+                high_resolution_clock::time_point start = high_resolution_clock::now();
+                isAvail=search(wordlist[word], board,row,col, &dir, &count);
+                high_resolution_clock::time_point end = high_resolution_clock::now();
+                duration<double> diff=duration_cast<duration<double>>(end-start);
+                totalcount+=count;
+                totaldiff+=diff.count();
+                if (isAvail){
+                    printOutput(wordlist[word], board, row, col, dir);
+                    break;
+                }
+            }
+            if (isAvail) break;
+
+        }
+        if (!isAvail) {
+            cout << "Word not found!\n\n";
+        }
+    }
+    cout << "Number of letter comparison: " << totalcount << " times\n";
+    cout << "Word search program runtime: " << totaldiff*1000 << " ms \n\n";
+}
